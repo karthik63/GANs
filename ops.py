1d8138fedcc2e8a7ac85cca95a333b2f869aa489
karthik63 <twoindeed@gmail.com>
2017-06-10 15:23:24 +0530

    Finished discriminator function

--- ops.py
+++ ops.py
@@ -4,7 +4,7 @@ import numpy as np
 def conv2d(a, b, c, d):
     return tf.nn.conv2d(a, b, c, d)
 
-def concatenate_conditioning_vector_with_feature_map(x, y)
+def concatenate_conditioning_vector_with_feature_map(x, y):
     return tf.concat([x, y*tf.constant([1])], axis=3)
 
 def lrelu(x, leak=0.2):
